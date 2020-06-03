f7218a4cebe1593ebb201ae86e0a6823df463831
dcherian <deepak@cherian.net>
2020-06-03 11:03:33 -0600

    Add proper plotting tests

--- cf_xarray/tests/test_accessor.py
+++ cf_xarray/tests/test_accessor.py
@@ -68,19 +68,24 @@ def test_args_methods(obj):
 @pytest.mark.parametrize("obj", dataarrays)
 def test_dataarray_plot(obj):
 
-    obj.isel(time=1).cf.plot(x="X", y="Y")
+    rv = obj.isel(time=1).cf.plot(x="X", y="Y")
+    assert isinstance(rv, mpl.collections.QuadMesh)
     plt.close()
 
-    obj.isel(time=1).cf.plot.contourf(x="X", y="Y")
+    rv = obj.isel(time=1).cf.plot.contourf(x="X", y="Y")
+    assert isinstance(rv, mpl.contour.QuadContourSet)
     plt.close()
 
-    obj.cf.plot(x="X", y="Y", col="T")
+    rv = obj.cf.plot(x="X", y="Y", col="T")
+    assert isinstance(rv, xr.plot.FacetGrid)
     plt.close()
 
-    obj.cf.plot.contourf(x="X", y="Y", col="T")
+    rv = obj.cf.plot.contourf(x="X", y="Y", col="T")
+    assert isinstance(rv, xr.plot.FacetGrid)
     plt.close()
 
-    obj.isel(lat=[0, 1], lon=1).cf.plot.line(x="T", hue="Y")
+    rv = obj.isel(lat=[0, 1], lon=1).cf.plot.line(x="T", hue="Y")
+    assert all([isinstance(line, mpl.lines.Line2D) for line in rv])
     plt.close()
 
 

a742dfeec03d8d40550f14e520bf480ba35491dc
dcherian <deepak@cherian.net>
2020-06-23 17:04:20 -0600

    Add datasets.py

---
+++ cf_xarray/tests/datasets.py
@@ -0,0 +1,49 @@
+import numpy as np
+import xarray as xr
+
+airds = xr.tutorial.open_dataset("air_temperature").isel(time=slice(4), lon=slice(50))
+airds.air.attrs["cell_measures"] = "area: cell_area"
+airds.air.attrs["standard_name"] = "air_temperature"
+airds.coords["cell_area"] = (
+    xr.DataArray(np.cos(airds.lat * np.pi / 180))
+    * xr.ones_like(airds.lon)
+    * 105e3
+    * 110e3
+)
+
+ds_no_attrs = airds.copy(deep=True)
+for variable in ds_no_attrs.variables:
+    ds_no_attrs[variable].attrs = {}
+
+
+popds = xr.Dataset()
+popds.coords["TLONG"] = (
+    ("nlat", "nlon"),
+    np.ones((20, 30)),
+    {"axis": "X", "units": "degrees_east"},
+)
+popds.coords["TLAT"] = (
+    ("nlat", "nlon"),
+    2 * np.ones((20, 30)),
+    {"axis": "Y", "units": "degrees_north"},
+)
+popds.coords["ULONG"] = (
+    ("nlat", "nlon"),
+    0.5 * np.ones((20, 30)),
+    {"axis": "X", "units": "degrees_east"},
+)
+popds.coords["ULAT"] = (
+    ("nlat", "nlon"),
+    2.5 * np.ones((20, 30)),
+    {"axis": "Y", "units": "degrees_north"},
+)
+popds["UVEL"] = (
+    ("nlat", "nlon"),
+    np.ones((20, 30)) * 15,
+    {"coordinates": "ULONG ULAT", "standard_name": "sea_water_x_velocity"},
+)
+popds["TEMP"] = (
+    ("nlat", "nlon"),
+    np.ones((20, 30)) * 15,
+    {"coordinates": "TLONG TLAT", "standard_name": "sea_water_potential_temperature"},
+)

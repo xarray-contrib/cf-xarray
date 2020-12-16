27c0a61a5b9f7a4d51913611f70dd8eeb9b54a5b
Julia Kent <jkent@ucar.edu>
2020-12-16 10:54:44 -0700

    facetgrid.FacetGrid to wrapped classes

--- cf_xarray/accessor.py
+++ cf_xarray/accessor.py
@@ -28,7 +28,7 @@ _WRAPPED_CLASSES = (
     xr.core.rolling.Rolling,
     xr.core.rolling.Coarsen,
     xr.core.weighted.Weighted,
-    xr.plot.FacetGrid,
+    xr.plot.facetgrid.FacetGrid,
 )
 
 #:  `axis` names understood by cf_xarray

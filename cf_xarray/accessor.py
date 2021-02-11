f00da4579e2b00c3cb1577fc6fe78aca5b87423e
malmans2 <mattia.almansi@jhu.edu>
2021-02-11 18:56:15 +0000

    add get dims, coords, indexes

--- cf_xarray/accessor.py
+++ cf_xarray/accessor.py
@@ -249,7 +249,9 @@ def _get_groupby_time_accessor(var: Union[DataArray, Dataset], key: str) -> List
         return []
 
 
-def _get_axis_coord(var: Union[DataArray, Dataset], key: str) -> List[str]:
+def _get_axis_coord(
+    var: Union[DataArray, Dataset], key: str, error: bool = True
+) -> List[str]:
     """
     Translate from axis or coord name to variable name
 
@@ -283,7 +285,7 @@ def _get_axis_coord(var: Union[DataArray, Dataset], key: str) -> List[str]:
     """
 
     valid_keys = _COORD_NAMES + _AXIS_NAMES
-    if key not in valid_keys:
+    if error and key not in valid_keys:
         raise KeyError(
             f"cf_xarray did not understand key {key!r}. Expected one of {valid_keys!r}"
         )
@@ -311,6 +313,56 @@ def _get_axis_coord(var: Union[DataArray, Dataset], key: str) -> List[str]:
                     results.update((coord,))
 
     return list(results)
+
+
+def _get_all(var: Union[DataArray, Dataset], key: str) -> List[str]:
+
+    results = set(_get_axis_coord(var, key, error=False))
+    for func in (_get_measure, _get_with_standard_name):
+        results.update(func(var, key))
+
+    return list(results)
+
+
+def _get_dims(var: Union[DataArray, Dataset], key: str) -> List[str]:
+    results = set(_get_all(var, key)).intersection(var.dims)
+    return list(results)
+
+
+def _get_single_dim(var: Union[DataArray, Dataset], key: str) -> List[str]:
+    return _get_single(var, key, _get_dims)
+
+
+def _get_coords(var: Union[DataArray, Dataset], key: str) -> List[str]:
+    results = set(_get_all(var, key)).intersection(var.coords)
+    return list(results)
+
+
+def _get_single_coord(var: Union[DataArray, Dataset], key: str) -> List[str]:
+    return _get_single(var, key, _get_coords)
+
+
+def _get_indexes(var: Union[DataArray, Dataset], key: str) -> List[str]:
+    results = set(_get_all(var, key)).intersection(var.indexes)
+    return list(results)
+
+
+def _get_single_index(var: Union[DataArray, Dataset], key: str) -> List[str]:
+    return _get_single(var, key, _get_indexes)
+
+
+def _get_single(var: Union[DataArray, Dataset], key: str, func):
+
+    results = func(var, key)
+
+    if len(results) > 1:
+        raise KeyError(
+            f"Multiple results for {key!r} found: {results!r}. I expected only one."
+        )
+    elif len(results) == 0:
+        raise KeyError(f"No results found for {key!r}.")
+
+    return results
 
 
 def _get_measure_variable(
@@ -374,34 +426,30 @@ def _get_with_standard_name(
 
 #: Default mappers for common keys.
 _DEFAULT_KEY_MAPPERS: Mapping[str, Tuple[Mapper, ...]] = {
-    "dim": (_get_axis_coord, _get_with_standard_name),
-    "dims": (_get_axis_coord, _get_with_standard_name),  # transpose
-    "drop_dims": (_get_axis_coord, _get_with_standard_name),  # drop_dims
-    "dimensions": (_get_axis_coord, _get_with_standard_name),  # stack
-    "dims_dict": (_get_axis_coord, _get_with_standard_name),  # swap_dims, rename_dims
-    "shifts": (_get_axis_coord, _get_with_standard_name),  # shift, roll
-    "pad_width": (_get_axis_coord, _get_with_standard_name),  # shift, roll
-    "names": (
-        _get_axis_coord,
-        _get_measure,
-        _get_with_standard_name,
-    ),  # set_coords, reset_coords, drop_vars
-    "labels": (_get_axis_coord, _get_measure, _get_with_standard_name),  # drop
-    "coords": (_get_axis_coord, _get_with_standard_name),  # interp
-    "indexers": (_get_axis_coord, _get_with_standard_name),  # sel, isel, reindex
+    "dim": (_get_dims,),
+    "dims": (_get_dims,),  # transpose
+    "drop_dims": (_get_dims,),  # drop_dims
+    "dimensions": (_get_dims,),  # stack
+    "dims_dict": (_get_dims,),  # swap_dims, rename_dims
+    "shifts": (_get_dims,),  # shift, roll
+    "pad_width": (_get_dims,),  # shift, roll
+    "names": (_get_all,),  # set_coords, reset_coords, drop_vars
+    "labels": (_get_all,),  # drop
+    "coords": (_get_coords,),  # interp
+    "indexers": (_get_indexes,),  # sel, isel, reindex
     # "indexes": (_get_axis_coord,),  # set_index
-    "dims_or_levels": (_get_axis_coord, _get_with_standard_name),  # reset_index
-    "window": (_get_axis_coord, _get_with_standard_name),  # rolling_exp
-    "coord": (_get_axis_coord_single,),  # differentiate, integrate
+    "dims_or_levels": (_get_indexes,),  # reset_index
+    "window": (_get_dims,),  # rolling_exp
+    "coord": (_get_coords,),  # differentiate, integrate
     "group": (
         _get_axis_coord_single,
         _get_groupby_time_accessor,
         _get_with_standard_name,
     ),
-    "indexer": (_get_axis_coord_single,),  # resample
-    "variables": (_get_axis_coord, _get_with_standard_name),  # sortby
+    "indexer": (_get_single_index,),  # resample
+    "variables": (_get_all,),  # sortby
     "weights": (_get_measure_variable,),  # type: ignore
-    "chunks": (_get_axis_coord, _get_with_standard_name),  # chunk
+    "chunks": (_get_dims,),  # chunk
 }
 
 
@@ -907,7 +955,7 @@ class CFAccessor:
         # these are valid for .sel, .isel, .coarsen
         all_mappers = ChainMap(
             key_mappers,
-            dict.fromkeys(var_kws, (_get_axis_coord, _get_with_standard_name)),
+            dict.fromkeys(var_kws, (_get_all,)),
         )
 
         for key in set(all_mappers) & set(kwargs):

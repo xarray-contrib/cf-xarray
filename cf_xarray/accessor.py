54b897a5afed5aea2f6fca3d89ad282bfe549aff
Pascal Bourgault <pascal.bourgault@gmail.com>
2021-02-24 14:59:40 -0500

    Do not return <this-array> instances (#180)

--- cf_xarray/accessor.py
+++ cf_xarray/accessor.py
@@ -351,7 +351,7 @@ def _get_with_standard_name(
 
     varnames = []
     if isinstance(obj, DataArray):
-        obj = obj._to_temp_dataset()
+        obj = obj.coords.to_dataset()
     for vname, var in obj.variables.items():
         stdname = var.attrs.get("standard_name", None)
         if stdname == name:

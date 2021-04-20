fb352cfeec19cf5890906ad25097f47c6f9f472c
Deepak Cherian <dcherian@users.noreply.github.com>
2021-04-20 10:26:23 -0600

    Add __version__ (#208)
    Co-authored-by: Anderson Banihirwe <axbanihirwe@ualr.edu>

--- cf_xarray/__init__.py
+++ cf_xarray/__init__.py
@@ -1,2 +1,10 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
 from .accessor import CFAccessor  # noqa
 from .helpers import bounds_to_vertices, vertices_to_bounds  # noqa
+
+try:
+    __version__ = get_distribution("cf_xarray").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = 'unknown'

7cbc4e24d425ad7dbdae4e757b798e1cd570cfcc
malmans2 <mattia.almansi@jhu.edu>
2021-06-10 17:25:28 +0100

    fix_version

--- cf_xarray/__init__.py
+++ cf_xarray/__init__.py
@@ -2,4 +2,4 @@ from .accessor import CFAccessor  # noqa
 from .helpers import bounds_to_vertices, vertices_to_bounds  # noqa
 from .utils import _get_version
 
-__version__ = _get_version
+__version__ = _get_version()

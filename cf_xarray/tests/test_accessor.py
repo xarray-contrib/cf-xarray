b6553b4f341a11f52552be4dc80aeeedb27c5a9e
Kristen Thyng <kristen@axds.co>
2021-06-23 10:38:18 -0500

    added tuple and list tests, not working

--- cf_xarray/tests/test_accessor.py
+++ cf_xarray/tests/test_accessor.py
@@ -1251,7 +1251,10 @@ def test_custom_criteria():
             "standard_name": "wind_speed$",
         },
     }
+    my_custom_criteria2 = {"temp": {"name": "temperature"}}
     cf_xarray.accessor.set_options(my_custom_criteria)
+    my_custom_criteria_list = [my_custom_criteria, my_custom_criteria2]
+    my_custom_criteria_tuple = (my_custom_criteria, my_custom_criteria2)
 
     # Match by name regex match
     ds = xr.Dataset()
@@ -1296,6 +1299,18 @@ def test_custom_criteria():
     assert_identical(
         ds.cf[["ssh"]], ds[["sea_surface_elevation", "sea_surface_height"]]
     )
+
+    # test criteria list of dicts
+    cf_xarray.accessor.set_options(my_custom_criteria_list)
+    ds = xr.Dataset()
+    ds["temperature"] = ("dim", np.arange(10))
+    assert_identical(ds.cf["temp"], ds["temperature"])
+
+    # test criteria tuple of dicts
+    cf_xarray.accessor.set_options(my_custom_criteria_tuple)
+    ds = xr.Dataset()
+    ds["temperature"] = ("dim", np.arange(10))
+    assert_identical(ds.cf["temp"], ds["temperature"])
 
 
 def test_cf_standard_name_table_version():

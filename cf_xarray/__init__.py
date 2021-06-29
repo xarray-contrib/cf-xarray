1411d872c0b9b4da74b47aa4b28d143455148756
Kristen Thyng <kthyng@gmail.com>
2021-06-29 10:53:22 -0500

    Update cf_xarray/__init__.py
    silence the flake8 warning, from @dcherian
    
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>

--- cf_xarray/__init__.py
+++ cf_xarray/__init__.py
@@ -1,6 +1,6 @@
 from .accessor import CFAccessor  # noqa
 from .helpers import bounds_to_vertices, vertices_to_bounds  # noqa
-from .options import set_options
+from .options import set_options  # noqa
 from .utils import _get_version
 
 __version__ = _get_version()

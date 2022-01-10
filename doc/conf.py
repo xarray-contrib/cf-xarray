f9fd58eeab379cb091eef2351987f36e6d7f7736
dcherian <deepak@cherian.net>
2022-01-10 11:31:56 -0700

    Add custom css

--- doc/conf.py
+++ doc/conf.py
@@ -198,6 +198,8 @@ html_logo = "_static/logo.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["style.css"]
+
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.

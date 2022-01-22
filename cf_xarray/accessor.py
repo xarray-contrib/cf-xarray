9a7582f55a6479db9aea06a4f7e9bd97ea35d999
dcherian <deepak@cherian.net>
2022-01-21 17:30:51 -0700

    Avoid setting 1D x,y where that dimension clashes with hue.

--- cf_xarray/accessor.py
+++ cf_xarray/accessor.py
@@ -765,7 +765,7 @@ def _possible_x_y_plot(obj, key, skip=None):
             elif ax_coord_name:
                 values = [v for v in values if v in ax_coord_name]
 
-            values = [v for v in values if v != skip]
+            values = [v for v in values if v != skip and obj[v].dims != (skip,)]
             if len(values) == 1 and not is_scalar(accessor._obj[values[0]]):
                 return values[0]
             else:

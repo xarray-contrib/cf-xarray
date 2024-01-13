f6c8a1f6001f8607a7c65a600660427fe91ecbf9
MichaelStLaurent-NOAA <6819509+mps01060@users.noreply.github.com>
2024-01-13 17:24:29 -0500

    Add larger bit depth for displaying flag_masks (#492)
    * Add flag_mask formatting support for bitdepth greater than uint8
    
    Underlying cf flag_mask codes (eg. == and .isin) work with
    flag_masks with more than 8 independent flags (eg. uint32), however
    the formatting codes that print messages to the screen are limited
    to uint8. Add/modify functions to unpack bits from larger integer
    dtypes.
    
    * Edit fixed-width formatting for larger bit depth flag_masks
    
    Original formatting widths work well for 8-bit flag_masks, but need
    some modification for larger integer values.
    
    * Fix bit_length calculating multiple times
    
    bit_length is re-calculated a second time inside _unpacked_bits,
    which can lead to incorrect bit_length. Pass this value as an
    argument instead, since it was properly calculated earlier in
    the process.
    
    * Remove unsigned integer check
    
    Flags do not have to be unsigned. For example, "basin" test dataset
    will fail the unsigned integer test even though it contains valid
    int64 flags.
    
    * Add test for 16bit version of flag_masks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add flag_indep_uint16 example dataset
    
    * One more test
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Deepak Cherian <deepak@cherian.net>

--- cf_xarray/tests/test_accessor.py
+++ cf_xarray/tests/test_accessor.py
@@ -26,6 +26,7 @@ from ..datasets import (
     dsg,
     flag_excl,
     flag_indep,
+    flag_indep_uint16,
     flag_mix,
     forecast,
     mollwds,
@@ -164,6 +165,7 @@ def test_repr() -> None:
     # Flag DataArray
     assert "Flag Variable" in repr(flag_excl.cf)
     assert "Flag Variable" in repr(flag_indep.cf)
+    assert "Flag Variable" in repr(flag_indep_uint16.cf)
     assert "Flag Variable" in repr(flag_mix.cf)
     assert "Flag Variable" in repr(basin.cf)
 
@@ -1837,6 +1839,30 @@ class TestFlags:
             res = flag_indep.cf.flags[name]
             np.testing.assert_equal(res.to_numpy(), expected[i])
 
+    def test_flag_indep_uint16(self) -> None:
+        expected = [
+            [True, False, False, False, False, True],  # bit     1
+            [False, True, False, False, False, True],  # bit     2
+            [False, False, True, False, False, True],  # bit     4
+            [False, True, False, True, False, True],  # bit     8
+            [False, False, False, False, True, True],  # bit    16
+            [False, False, True, True, False, True],  # bit    32
+            [False, False, True, True, False, True],  # bit    64
+            [False, False, False, True, False, True],  # bit   128
+            [False, False, False, True, True, True],  # bit   256
+            [False, False, False, True, True, True],  # bit   512
+            [False, False, False, False, True, True],  # bit  1024
+            [False, False, False, False, False, True],  # bit  2048
+            [False, False, False, False, False, True],  # bit  4096
+            [False, False, False, False, True, True],  # bit  8192
+            [False, False, False, False, False, True],  # bit 16384
+            [False, False, False, False, False, True],  # bit 32768
+        ]
+        for i in range(16):
+            name = f"flag_{2**i}"
+            res = flag_indep_uint16.cf.flags[name]
+            np.testing.assert_equal(res.to_numpy(), expected[i])
+
     def test_flag_mix(self) -> None:
         expected = [
             [False, False, True, True, False, False, True, True],  # flag 1
@@ -1983,6 +2009,7 @@ def test_curvefit() -> None:
         [basin, "Flag Variable"],
         [flag_mix, "Flag Variable"],
         [flag_indep, "Flag Variable"],
+        [flag_indep_uint16, "Flag Variable"],
         [flag_excl, "Flag Variable"],
         [dsg, "Discrete Sampling Geometry"],
     ),

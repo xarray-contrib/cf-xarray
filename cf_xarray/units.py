997ea4b6d1969c5c9ab2dc97faa1c9e078427c93
Deepak Cherian <dcherian@users.noreply.github.com>
2024-01-31 09:54:21 -0700

    Update cf_xarray/units.py

--- cf_xarray/units.py
+++ cf_xarray/units.py
@@ -80,6 +80,7 @@ units = pint.UnitRegistry(
 # ----- end block copied from metpy
 
 # need to insert to make sure this is the first preprocessor
+# This ensures we convert integer `1` to string `"1"`, as needed by pint.
 units.preprocessors.insert(0, str)
 
 # -----

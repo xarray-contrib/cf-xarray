c656abd403ba3e74aa39464ea246ce1c3b945a99
Deepak Cherian <dcherian@users.noreply.github.com>
2025-03-06 13:59:27 -0700

    Add FlagGrouper (#556)
    * Add FlagGrouper
    
    Closes #472
    
    * fix
    
    * fix doc build
    
    * update
    
    * cleanup
    
    * try again

---
+++ cf_xarray/groupers.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from xarray.groupers import EncodedGroups, UniqueGrouper
+
+
+@dataclass
+class FlagGrouper(UniqueGrouper):
+    def factorize(self, group) -> EncodedGroups:
+        if "flag_values" not in group.attrs or "flag_meanings" not in group.attrs:
+            raise ValueError(
+                "FlagGrouper can only be used with flag variables that have"
+                "`flag_values` and `flag_meanings` specified in attrs."
+            )
+
+        values = np.array(group.attrs["flag_values"])
+        full_index = pd.Index(group.attrs["flag_meanings"].split(" "))
+
+        self.labels = values
+
+        # TODO: we could optimize here, since `group` is already factorized,
+        # but there are subtleties. For example, the attrs must be up to date,
+        # any value that is not in flag_values will cause an error, etc.
+        ret = super().factorize(group)
+
+        ret.codes.attrs.pop("flag_values")
+        ret.codes.attrs.pop("flag_meanings")
+
+        return EncodedGroups(
+            codes=ret.codes,
+            full_index=full_index,
+            group_indices=ret.group_indices,
+        )

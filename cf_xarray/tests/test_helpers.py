cfe87d0bcdce241c2387460fa9147a390e4b6a19
Tom Vo <tomvothecoder@gmail.com>
2025-08-11 07:46:49 -0700

    Update `bounds_to_vertices` to handle descending arrays (#579)
    * Update `bounds_to_vertices` to handle descending arrays
    Add `_get_ordered_vertices()` to handle extraction of unique vertex values in ascending order
    
    * Update docstring
    
    * Update `_get_ordered_vertices()` to preserve order of values
    
    * Fix `_get_ordered_vertices()` to preserve leading dims
    - Also handle strictly monotonic bounds
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix casting issue with numpy 1.26.4
    
    * Update docstrings for clarity
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix logic for checking strictly monotonic
    
    * Fix logic for checking strictly monotonic
    
    * Delete qa/mvce.py
    
    * Update `bounds_to_vertices to sort on coordinates
    - Add `_get_core_dim_orders()` to get core dim orders for sorting vertices
    - Update `_bounds_helper()` to accept `core_dim_orders` param
    - Update `_get_ordered_vertices()` to sort on `core_dim_orders` param
    - Fix `_get_ordered_vertices()` to get correct array values for vertices based on order of core dims
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix 3d vertices test
    
    * Add typecasting to prevent numpy 1.26.4 issue
    
    * Add typecasting to prevent numpy 1.26.4 issue
    
    * Add typecasting to prevent numpy 1.26.4 issue
    
    * Fix mypy error expecting bool not np.bool
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- cf_xarray/tests/test_helpers.py
+++ cf_xarray/tests/test_helpers.py
@@ -1,3 +1,4 @@
+import xarray as xr
 from numpy.testing import assert_array_equal
 from xarray.testing import assert_equal
 
@@ -12,7 +13,7 @@ except ImportError:
 
 
 def test_bounds_to_vertices() -> None:
-    # 1D case
+    # 1D case (stricly monotonic, descending bounds)
     ds = airds.cf.add_bounds(["lon", "lat", "time"])
     lat_c = cfxr.bounds_to_vertices(ds.lat_bounds, bounds_dim="bounds")
     assert_array_equal(ds.lat.values + 1.25, lat_c.values[:-1])
@@ -33,6 +34,59 @@ def test_bounds_to_vertices() -> None:
     )
     lon_no = cfxr.bounds_to_vertices(rotds.lon_bounds, bounds_dim="bounds", order=None)
     assert_equal(lon_no, lon_ccw)
+
+    # 2D case (monotonicly increasing coords, non-monotonic bounds)
+    bounds_2d_desc = xr.DataArray(
+        [[50.5, 50.0], [51.0, 50.5], [51.0, 50.5], [52.0, 51.5], [52.5, 52.0]],
+        dims=("lat", "bounds"),
+        coords={"lat": [50.75, 50.75, 51.25, 51.75, 52.25]},
+    )
+    expected_vertices_2d_desc = xr.DataArray(
+        [50.0, 50.5, 50.5, 51.5, 52.0, 52.5],
+        dims=["lat_vertices"],
+    )
+    vertices_2d_desc = cfxr.bounds_to_vertices(bounds_2d_desc, bounds_dim="bounds")
+    assert_equal(expected_vertices_2d_desc, vertices_2d_desc)
+
+    # 3D case (non-monotonic bounds, monotonicly increasing coords)
+    bounds_3d = xr.DataArray(
+        [
+            [
+                [50.0, 50.5],
+                [50.5, 51.0],
+                [51.0, 51.5],
+                [51.5, 52.0],
+                [52.0, 52.5],
+            ],
+            [
+                [60.0, 60.5],
+                [60.5, 61.0],
+                [61.0, 61.5],
+                [61.5, 62.0],
+                [62.0, 62.5],
+            ],
+        ],
+        dims=("extra", "lat", "bounds"),
+        coords={
+            "extra": [0, 1],
+            "lat": [0, 1, 2, 3, 4],
+            "bounds": [0, 1],
+        },
+    )
+    expected_vertices_3d = xr.DataArray(
+        [
+            [50.0, 50.5, 51.0, 51.5, 52.0, 52.5],
+            [60.0, 60.5, 61.0, 61.5, 62.0, 62.5],
+        ],
+        dims=("extra", "lat_vertices"),
+        coords={
+            "extra": [0, 1],
+        },
+    )
+    vertices_3d = cfxr.bounds_to_vertices(
+        bounds_3d, bounds_dim="bounds", core_dims=["lat"]
+    )
+    assert_equal(vertices_3d, expected_vertices_3d)
 
     # Transposing the array changes the bounds direction
     ds = mollwds.transpose("x", "y", "x_vertices", "y_vertices", "bounds")
